afc0b061cbb7653099d24e0659aa41f19ff07cb4
Torrence Brown <torrenceb90@gmail.com>
2021-01-03 00:48:26 -0700

    Added date inputs

--- src/components/UI/Booking/Booking.js
+++ src/components/UI/Booking/Booking.js
@@ -13,8 +13,11 @@ const Booking = () => {
       <div className="booking__form-container">
         <h3>WHERE DO YOU WANT TO GO?</h3>
         <form className="booking__form">
+          {/* TODO: Add inputs for dates */}
           <input placeholder="ORIGIN?" />
           <input placeholder="DESTINATION?" />
+          <input placeholder="DEPARTURE DATE"/>
+          <input placeholder="RETURN DATE"/>
           <button className="booking__form-button">SEND IT</button>
         </form>
       </div>
