7e3a0c6b24c590577c8ad7c380aa9597f849895e
Torrence Brown <torrenceb90@gmail.com>
2021-01-06 10:21:34 -0800

    Added in flight client parameters

---
+++ src/api/flight-client.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+// Base URL for flight requests
+const flightClient = ({query, countryName, localCurrency, locale }) => {
+  const API_KEY = process.env.REACT_APP_FLIGHT_KEY;
+  const options = {
+    method: "GET",
+    url:
+      "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    params: {
+      query: query,
+      country: countryName,
+      currency: localCurrency,
+      locale: locale,
+    },
+    headers: {
+      "x-rapidapi-key": `${API_KEY}`,
+      "x-rapidapi-host":
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+    },
+  };
+  const instance = axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      Error(error);
+    });
+  console.log(instance);
+};
+
+export default flightClient;
